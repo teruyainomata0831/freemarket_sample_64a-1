e346b2046bd15cb548af6b4a62c96b52767bf9cc
yu-jin0 <pakkuncho085@gmail.com>
2019-12-09 10:38:38 +0900

    ホームページのクレジットカード画面途中

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,10 @@
 class PostsController < ApplicationController
   def index
   end
+
+  def credit
+  end
+
+  def addcard
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'posts#index'
+
+  get "posts/credit" => "posts#credit"
+  get "posts/addcard" => "posts#addcard"
 end
