82bc47c6131463dea4985c837420483f252550a1
kakiwo <kakiwo@hotmail.co.jp>
2019-12-11 17:50:02 +0900

    edit-signup-validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,10 @@ class User < ApplicationRecord
   devise :validatable, password_length: 7..128
   devise :validatable, email_regexp: /\A[^@\s]+@[^@\s]+\z/
 
+  validates :first_name, :last_name, presence: true, format: { with: /\A[(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\p{blank}ー－]+\z/}
   validates :first_name_kana, :last_name_kana, presence: true, format: { with: /\A[\p{katakana}\p{blank}ー－]+\z/, message: 'はカタカナで入力して下さい。'}
-  validates :nickname, :last_name, :first_name, :birth_year, :birth_month, :birth_day, presence: true
+  validates :birth_year, :birth_month, :birth_day, presence: true, numericality: { greater_than: 0 }
+  validates :nickname, presence: true
   
   has_many :items
 end
