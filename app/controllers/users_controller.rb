c31fca4cc18b1f232528501f50d76cb27875ee34
teruyainomata0831 <terubot1711@icloud.comgit config --global user.name teruyainomata0831>
2019-12-11 20:38:40 +0900

    mypage-right

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def mypage
   end
+
+  def mypage_list
+  end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'users/mypage'
   get 'users/mypage_side'
-  get 'users/my'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "posts#index"
+  resources :users, only:[:show] do
+    collection do
+      get :mypage_list
+      get :mypage
+    end
+  end
 end
