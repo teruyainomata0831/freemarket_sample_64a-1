c6bfef0d396f0171aafd5a5e192e3b1ae06b3c3f
teruyainomata0831 <terubot1711@icloud.comgit config --global user.name teruyainomata0831>
2019-12-11 17:19:44 +0900

    mypage-right

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users/mypage'
   get 'users/mypage_side'
+  get 'users/my'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "posts#index"
