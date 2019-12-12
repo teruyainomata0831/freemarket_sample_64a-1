8a7cf9c5ef4e6039408589df2fb6db2f8186cf99
teruyainomata0831 <terubot1711@icloud.comgit config --global user.name teruyainomata0831>
2019-12-12 15:04:15 +0900

    mypage-right

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -2,11 +2,11 @@ jQuery(function($){
   $('.notice').click(function(){
       $('.active').removeClass('active');
       $(this).addClass('active');
-      $('.show').removeClass('show');
+      $('.test').removeClass('test');
       // クリックしたタブからインデックス番号を取得
       const index = $(this).index();
       // クリックしたタブと同じインデックス番号をもつコンテンツを表示
-      $('.tab-content').eq(index).addClass('show');
+      $('.tab-content').eq(index).addClass('test');
   });
 
   $('.transaction').click(function(){
