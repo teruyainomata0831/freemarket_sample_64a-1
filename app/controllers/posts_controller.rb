79836f36ec0e3bafa176db6ebd330ba7b731c428
hiro8190 <kdr.8190@gmail.com>
2019-12-12 12:54:22 +0900

    create goods list step1

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
+    render 'goodsDetail'
   end
 
   def step1
@@ -18,5 +19,8 @@ class PostsController < ApplicationController
   end
 
   def register
+  end
+
+  def goodsDetail
   end
 end
