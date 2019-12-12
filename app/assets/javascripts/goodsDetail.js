8f8640e73f7f9e933b3286432b704aa01bd72595
yu-jin0 <pakkuncho085@gmail.com>
2019-12-12 20:39:02 +0900

    update

--- app/assets/javascripts/goodsDetail.js
+++ app/assets/javascripts/goodsDetail.js
@@ -1,5 +1,5 @@
 $(function(){
-
+  
   $('.item-dots img').hover(function(){
     let index = $(".item-dots img").index(this);
       $('.is-active').removeClass('is-active');
@@ -8,5 +8,5 @@ $(function(){
   $('.item-dots img').hover(function(){
     let src = $(this).attr('src');
       $('.main-img').attr('src', src);
-  });
+  })
 })
