97ff4c902961c506d9c9466582ba64bb20be5618
teruyainomata0831 <terubot1711@icloud.comgit config --global user.name teruyainomata0831>
2019-12-14 16:57:38 +0900

    mypage-exhibits完成
    出品した商品ページ完成しました！

--- app/assets/javascripts/exhibit1.js
+++ app/assets/javascripts/exhibit1.js
@@ -1,11 +1,11 @@
 jQuery(function($){
-  $('.active').click(function(){
-      $('.active.active').removeClass('active.active');
-      $(this).addClass('active.active');
+  $('.active-test').click(function(){
       $('.active').removeClass('active');
+      $(this).addClass('active');
+      $('.show-test-jquery').removeClass('show-test-jquery');
       // クリックしたタブからインデックス番号を取得
       const index = $(this).index();
       // クリックしたタブと同じインデックス番号をもつコンテンツを表示
-      $('.tab-content').eq(index).addClass('active');
+      $(' .tab').eq(index).addClass('show-test-jquery');
   });
 });
