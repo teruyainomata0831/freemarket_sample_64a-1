343d9ad5e7a3ff41223f837d4cf4b3b84e476445
yu-jin0 <pakkuncho085@gmail.com>
2019-12-16 16:09:45 +0900

    step5画像調整

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,41 +1,42 @@
 class PostsController < ApplicationController
   def index
+    render 'step5'
   end
 
   def editprofile
-    render = "editprofile"
+
   end
 
   def goodsBuy
-    render = "goodsBuy"
+
   end
   
   def goodsDetail
-    render = "goodsDetail"
+
   end
   
   def register
-    render = "register"
+   
   end
 
   def step1
-    render = "step1"
+
   end
 
   def step2
-    render = "step2"
+  
   end
 
   def step3
-    render = "step3"
+
   end
 
   def step4
-    render = "step4"
+
   end
 
   def step5
-    render = "step5"
+
   end
 
 end
