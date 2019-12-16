e52c7ece5397c8f600b15f9f71580d225c36bc1c
kakiwo <kakiwo@hotmail.co.jp>
2019-12-16 18:57:31 +0900

    EDIT-address

--- app/controllers/address_controller.rb
+++ app/controllers/address_controller.rb
@@ -1,2 +1,27 @@
 class AddressController < ApplicationController
+
+  def new
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+    if @address.save
+      redirect_to new_credit_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(
+      :postcode,
+      :prefecture,
+      :city,
+      :block,
+      :building,
+    )
+  end
+
 end
