943046d0e68c4dfdaaea1c399adbb064e6b6b4c4
kakiwo <kakiwo@hotmail.co.jp>
2019-12-16 13:56:57 +0900

    edit-views-files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191210093227) do
+ActiveRecord::Schema.define(version: 20191212121408) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "postcode",   null: false
@@ -89,6 +89,8 @@ ActiveRecord::Schema.define(version: 20191210093227) do
     t.datetime "remember_created_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "provider"
+    t.string   "uid"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
