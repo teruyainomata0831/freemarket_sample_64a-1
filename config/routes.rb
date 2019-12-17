99057b2bc00da673c7ccce16f9df23ea402a3e06
kakiwo <kakiwo@hotmail.co.jp>
2019-12-17 21:28:45 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
     resources :signup
+  end
   resources :address, only: [ :new, :create ]
   resources :credit, only: :index do
     collection do
