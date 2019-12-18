517864a55a0ed5ea722fa22f082e1e07c06d6d08
yu-jin0 <pakkuncho085@gmail.com>
2019-12-18 19:52:20 +0900

    update

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -11,7 +11,7 @@ class CardController < ApplicationController
       redirect_to action: "new"
     else
       customer = Payjp::Customer.create(card: params['payjp-token'] )
-      @card = Card.new(customer_id: customer.id, card_id: customer.default_card)
+      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
       if @card.save
         redirect_to action: "show"
       else
@@ -21,12 +21,9 @@ class CardController < ApplicationController
   end
 
   def show
-    def index #CardのデータをPayjpに送って情報を取り出す
-      if @card.present?
-        Payjp.api_key = "sk_test_f11178b5365a163734f1d128"
-        customer = Payjp::Customer.retrieve(@card.customer_id)
-        @card_information = customer.cards.retrieve(@card.card_id)
-      end
-    end
+    # card = Card.where(user_id: current_user.id)
+    # Payjp.api_key = 'sk_test_f11178b5365a163734f1d128'
+    #   customer = Payjp::Customer.retrieve(@card.customer_id)
+    #   @card_information = customer.cards.retrieve(@card.card_id)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
+    render 'card/new'
   end
 
   def editprofile
