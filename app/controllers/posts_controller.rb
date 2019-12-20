c25297e34d673d85db28a039edd0c0e3b49782f2
kakiwo <kakiwo@hotmail.co.jp>
2019-12-20 21:31:32 +0900

    delete-posts-views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,87 +1,19 @@
 class PostsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  # before_action :move_to_index, except: [:index, :show]
 
-  def index
-    render 'exhibit'
-  end
+  # def index
+  #   render 'exhibit'
+  # end
 
-  def editprofile
-    @user = User.find(params[:id])
-    @posts = @user.posts
-      render 'exhibit'
-  end
+  # def editprofile
+  #   @user = User.find(params[:id])
+  #   @posts = @user.posts
+  #     render 'exhibit'
+  # end
 
-
-
-  def goodsBuy
-  
-  end
-  
-  def goodsDetail
-
-  end
-  
-  def register
-
-  end
-
-  def step1
-
-  end
-
-  def step2
-    
-  end
-
-  def step3
-
-  end
-
-  def step4
-    
-  end
-
-  def pay
-    Payjp.api_key = ENV['PAYJP_ACCESS_KEY']
-    if params['payjp-token'].blank?
-      redirect_to action: "step4"
-    else
-      customer = Payjp::Customer.create(card: params['payjp-token'] )
-      @card = Card.new(user_id: current_user.id, customer_id: customer.id, card_id: customer.default_card)
-      if @card.save
-        redirect_to action: "step5"
-      else
-        redirect_to action: "pay"
-      end
-    end
-  end
-
-  def step5
-  
-  end
-
-  def editprofile
-  end
-
-
-
-  def goodsBuy
-  end
-
-
-  def creditInfo
-  end
-
-  def creditAdd
-  end
-
-  def creditConfirm
-  end
-
-  private
-  def move_to_index
-    redirect_to action: :index unless user_signed_in?
-  end
-
+  # private
+  # def move_to_index
+  #   redirect_to action: :index unless user_signed_in?
+  # end
 
 end
