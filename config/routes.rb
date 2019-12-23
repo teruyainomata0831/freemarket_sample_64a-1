067cd90f5832641591cbfb06e8afad770e5568ed
hiro8190 <kdr.8190@gmail.com>
2019-12-23 16:53:04 +0900

    sns認証完了

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root to: 'items#index'
 
   # ユーザー登録
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, :controllers => {
+    :omniauth_callbacks =>  "users/omniauth_callbacks"
+  }
   devise_scope :user do
     resources :signup
   end
