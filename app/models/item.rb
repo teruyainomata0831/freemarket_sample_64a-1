2ae129478c56dcd8bae6669eea434b51965e8ccd
zigrable <atu8815@gmail.com>
2019-12-24 20:27:14 +0900

    edit 追加7

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   has_many :images, dependent: :destroy
-  accepts_nested_attributes_for :images
+  accepts_nested_attributes_for :images, allow_destroy: true
   belongs_to :seller, class_name: 'User'
   
   validates :name, :description, :price, :region, presence: true
