cb5aeb04c6ce15617916574fd5e2f089ea7f5440
kakiwo <57390663+kakiwo@users.noreply.github.com>
2019-12-25 16:14:25 +0900

    Update goodsDetail.js

--- app/assets/javascripts/goodsDetail.js
+++ app/assets/javascripts/goodsDetail.js
@@ -1,12 +1,14 @@
-$(function(){
-  
-  $('.item-dots img').hover(function(){
-    var num = $(".item-dots img").index(this);
-      $('.is-active').removeClass('is-active');
-      $('.item-dot').eq(num).addClass('is-active');
-  });
-  $('.item-dots img').hover(function(){
-    var src = $(this).attr('src');
-      $('.main-img').attr('src', src);
+$(document).on('turbolinks:load', function() { 
+  $(function(){
+
+    $('.item-dots img').hover(function(){
+      var num = $(".item-dots img").index(this);
+        $('.is-active').removeClass('is-active');
+        $('.item-dot').eq(num).addClass('is-active');
+    });
+    $('.item-dots img').hover(function(){
+      var src = $(this).attr('src');
+        $('.main-img').attr('src', src);
+    })
   })
 })
